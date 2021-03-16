4294cf169b96efac5a871ce6fca3ec12ab1145fa
artur <arturluisoliveira@gmail.com>
2021-03-16 16:04:53 -0300

    add git ignore

--- Nano Terminal/main.swift
+++ Nano Terminal/main.swift
@@ -7,5 +7,45 @@
 
 import Foundation
 
-print("Hello, World!")
+func shell(_ command: String) -> String {
+    let task = Process()
+    let pipe = Pipe()
+    
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.launch()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+    
+    return output
+}
+//PC config
+//print(shell("system_profiler SPHardwareDataType"))
 
+//IP address
+//print(shell("ipconfig getifaddr en0"))
+
+//Download history
+//sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'select LSQuarantineDataURLString from LSQuarantineEvent'
+
+//shut down
+//sudo shutdown -h now
+
+//restart
+//$ sudo shutdown -r now
+
+
+//uptime
+//Uptime
+
+//external ip address
+//curl ipecho.net/plain; echo
+
+shell("say \"this is awesome\"")
+
+//talk
+// say "This Mac runs OS X, not OS ex"
+//say -f /path/to/file.txt
