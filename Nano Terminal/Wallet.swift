a4be50d064e91f11b1274d64850af39e0ee08742
Julia Alberti Maia <juliaalberti@MacBook-Pro-de-Julia.local>
2021-03-18 14:46:57 -0300

    adding Wallet

---
+++ Nano Terminal/Wallet.swift
@@ -0,0 +1,47 @@
+//
+//  Wallet.swift
+//  Nano Terminal
+//
+//  Created by Julia Alberti Maia on 17/03/21.
+//
+
+import Foundation
+
+enum InvestmentType{
+    case lci
+    case cdb
+    
+    var percentage: Double {
+        switch self {
+        case .lci:
+            return 0.2
+        case .cdb:
+            return 1.0
+        }
+    }
+    
+    func getPrevision(period: Int,value: Decimal) -> Decimal { //periodo = 0 tratar
+        var aux: Decimal
+        
+        aux = value
+        for _ in 1...period{
+            aux = aux + (aux * Decimal(percentage))
+        }
+        return aux
+    }
+}
+
+struct Wallet{
+    var period: Int
+    var value: Decimal
+    var investments: [InvestmentType:Decimal]
+    //[tipo de investimento:quanto quer investir]
+    func getGeneralPrevision(period: Int,investments: [InvestmentType:Decimal]) -> Decimal{
+        var nonInvestmentValue = value - investments.reduce(Decimal(0)){acumulator,current in acumulator + current.value}
+        
+        let prevision = investments.map{investment in
+            (investment.key: investment.key.getPrevision(period: period, value: investment.value))}
+        
+        var previsionAll = nonInvestmentValue + prevision.reduce(Decimal(0)){acumulator,current in acumulator + current[1]}
+    }
+}
