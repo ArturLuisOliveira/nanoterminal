5bb07c4c3d61080976c0700ea08460a23225a992
artur <arturluisoliveira@gmail.com>
2021-03-18 16:06:50 -0300

    fix main

--- Nano Terminal/main.swift
+++ Nano Terminal/main.swift
@@ -7,24 +7,8 @@
 
 import Foundation
 
-
-// artur
-
-//Printer.writeAndTalk("Type the string value:")
-//let stringValue = Input.readString()
-//
-//
-//Printer.writeAndTalk("Type double value:")
-//let decimalValue = Input.readDecimal()
-//
-//Printer.writeAndTalk("Thank you for typing \(stringValue) and \(decimalValue)")
-Terminal()
-
-// ----
-
-
-
-
 // julia
 
 //---
+
+let _ = Terminal()
